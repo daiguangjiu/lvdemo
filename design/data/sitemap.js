2437c596157b3b3a37e6dfcec4edc692a07f6782
daiguangjiu <ahhsxycb@163.com>
2013-08-21 18:50:08 +0800

    design
    commit by daiguangjiu 18:50

---
+++ design/data/sitemap.js
@@ -0,0 +1,23 @@
+﻿var sitemap = 
+{
+"rootNodes":[
+{
+"pageName":"sitemap",
+"type":"Flow",
+"url":"sitemap.html"},
+{
+"pageName":"index",
+"type":"Wireframe",
+"url":"index.html"},
+{
+"pageName":"首页",
+"type":"Wireframe",
+"url":"首页.html"},
+{
+"pageName":"页面 3",
+"type":"Wireframe",
+"url":"页面_3.html"},
+{
+"pageName":"页面 2",
+"type":"Wireframe",
+"url":"页面_2.html"}]};
